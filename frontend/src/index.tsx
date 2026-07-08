2839ab4fc59180c310aa9a2c7f6454c869d274e9
ljspinelli <ljspinelli.pa@gmail.com>
2026-07-08 11:31:46 -0300

    Initialize React app in index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,1 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/theme.css";
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(<App />);
+}
 
