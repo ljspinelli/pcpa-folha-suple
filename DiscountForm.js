8f75f94e9d50d1d24c5bb0f36afee32714573eb3
ljspinelli <ljspinelli.pa@gmail.com>
2026-08-07 12:09:41 -0300

    Replace total formatting with custom function

--- DiscountForm.js
+++ DiscountForm.js
@@ -397,7 +397,7 @@ function DiscountForm({
             border: "1px solid #e0e0e0"
           }}
         >
-          Total: R$ {total.toLocaleString("pt-BR", { minimumFractionDigits: 2 })}
+          Total: R$ {formatarNumeroParaMoeda(total)}
         </div>
       </div>
 
